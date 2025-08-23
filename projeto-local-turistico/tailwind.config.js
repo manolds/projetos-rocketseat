eef5e0bb4d29e2511c38bbaf4988f4a1794ba25c
Leonardo Souza <leonardomtzsouza@rede.ulbra.br>
2025-08-23 18:18:11 -0300

    Projetos rocketseat

---
+++ projeto-local-turistico/tailwind.config.js
@@ -0,0 +1,26 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        default: "#F5F4F0", // bg-default
+        primary: "#1B1B1B", // text-primary
+        secondary: "#333333",
+        tertiary: "#696969",
+        headline: "#E1624F",
+
+        border: {
+          primary: "#D9D9D9", // border-primary
+        },
+
+        accent: {
+          blue: "#0C51A7",
+          orange: "#E95E10",
+          pink: "#E5245E",
+          purple: "#591B98",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
